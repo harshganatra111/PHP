09ec9502a23304f1be84ee1a20b2dc94f7e39d1b
harshganatra111 <harshganatra111@gmail.com>
2020-02-17 14:00:56 +0530

    PHP-1

---
+++ embedphpinhtml.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Embed PHP in html</title>
+    </head>
+    <body>
+        <h1>Embed PHP in html</h1>
+        <p>
+        <?php
+           echo "This is a Paragraph" ;
+        ?>
+        
+        </p>
+    </body>
+</html>
